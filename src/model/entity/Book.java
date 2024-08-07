0feeebda9eb656e441b997b73eeb13a6384ac15f
carlos cruz <carturo99@gmail.com>
2024-08-06 20:02:38 -0500

    [MODIFIED] Update Entity Book

--- src/model/entity/Book.java
+++ src/model/entity/Book.java
@@ -1,5 +1,7 @@
 package model.entity;
 
+import util.Input;
+
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,10 +10,12 @@ public class Book extends Publication implements IVisualizable {
     private String isbn;
     private boolean isReaded;
     private int timeReaded;
+    private ArrayList<Page> pages;
 
-    public Book(String title, Date edititionDate, String editorial, String[] authors) {
+    public Book(String title, Date edititionDate, String editorial, String[] authors, ArrayList<Page> pages) {
         super(title, edititionDate, editorial);
         setAuthors(authors);
+        setPages(pages);
     }
 
     public int getId() {
@@ -42,6 +46,14 @@ public class Book extends Publication implements IVisualizable {
         this.timeReaded = timeReaded;
     }
 
+    public ArrayList<Page> getPages() {
+        return pages;
+    }
+
+    public void setPages(ArrayList<Page> pages) {
+        this.pages = pages;
+    }
+
     public String verifyState() {
         String state = "";
         if (isReaded) {
@@ -57,9 +69,33 @@ public class Book extends Publication implements IVisualizable {
         Date dateI = startToSee(new Date());
         stopToSee(dateI, new Date());
 
-        for (int i = 0;i < 1000; i++) {
-            System.out.println("...........................");
-        }
+        int i = 0;
+        Page page;
+
+        do {
+            page = getPages().get(i);
+            System.out.println(".........................");
+            System.out.println("Page " + page.getNumber());
+            System.out.println(page.getContent());
+            System.out.println(".........................");
+
+            if (i != 0) {
+                System.out.println("1. Back page");
+            }
+
+            System.out.println("2. Next page");
+            System.out.println("0. Close book\n");
+
+            int response = Input.validateInput(0, 2);
+
+            if (response == 2) {
+                i++;
+            } else if (response == 1) {
+                i--;
+            } else if (response == 0) {
+                break;
+            }
+        } while (i < getPages().size());
 
         System.out.println("\nYou viewed: " + toString());
         System.out.println("By: " + getTimeReaded() + "seconds");
@@ -97,6 +133,7 @@ public class Book extends Publication implements IVisualizable {
 
     public static ArrayList<Book> makeList() {
         ArrayList<Book> books = new ArrayList<>();
+        ArrayList<Page> pages = new ArrayList<>();
         String[] authors = new String[3];
 
         for (int i = 0; i < authors.length; i++) {
@@ -104,14 +141,54 @@ public class Book extends Publication implements IVisualizable {
         }
 
         for (int i = 0; i < 5; i++) {
+            pages.add(new Page((i + 1), "xxxxxxxxxxxxxxxxxxxx\nContent of page " + (i + 1) + "\nxxxxxxxxxxxxxxxxxxxx"));
+        }
+
+        for (int i = 0; i < 5; i++) {
             books.add(new Book(
                     "Book " + (i + 1),
                     new Date(),
                     "Editorial " + (i + 1),
-                    authors
+                    authors,
+                    pages
             ));
         }
 
         return books;
+    }
+
+    public static class Page {
+        private int id;
+        private int number;
+        private String content;
+
+        public Page(int number, String content) {
+            this.number = number;
+            this.content = content;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
     }
 }
